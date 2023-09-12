6529288f84c46a89191d7c4b1f2e9cab63b191f0
NikhilPachauri <141986088+NikhilPachauri@users.noreply.github.com>
2023-09-12 09:23:54 +0530

    Update Program.cs

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -1,8 +1,44 @@
-﻿using System;
-namespace lab_3
-{
-    class CS_Datatypes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ass3;
+
+namespace assimblydifferent
+{   // Q3 program
+    class Pro : Question_3
     {
-        
+        public static void Main(String[] vs)
+        {
+            Pro e1 = new Pro();
+            e1.name = "rohit";
+            e1.course = "bca";
+            Console.WriteLine(e1.Detail());
+            Console.ReadKey();
+
+        }
+    }
+
+    //Question 5
+    class Question_5A : Question_5
+    {
+        static void Main()
+        {
+            Question_5A c1 = new Question_5A();
+            c1.Display();
+            Console.ReadLine();
+        }
+    }
+
+    //Question 10
+    class pro10 : Question_10
+    {
+        static void Main()
+        {
+            Question_10 instance = new Question_10();
+            instance.publiMethod();
+        }
     }
 }
+
